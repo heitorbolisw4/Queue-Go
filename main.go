077e574ea671ec18bdc2e60ac94161670f7f2145
Heitor <heitorbolisw4@gmail.com>
2026-06-24 18:23:53 -0300

    initial logic for Enqueue Added

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import "fmt"
+
+func main() {
+	queue := Queue{}
+
+	queue.Enqueue("Heitor")
+	queue.Enqueue("Golang")
+	queue.Enqueue("Counter-Strike")
+
+	fmt.Println(queue.Dequeue())
+	fmt.Println(queue.Dequeue())
+	fmt.Println(queue.Dequeue())
+}
--- queue.go
+++ queue.go
@@ -8,3 +8,24 @@ type Node struct {
 	Val  string
 	Next *Node
 }
+
+func (q *Queue) Enqueue(name string) {
+	node := Node{Val: name}
+
+	if q.Head == nil {
+		q.Head = &node
+	} else {
+		q.Head.Next = &node
+	}
+}
+
+func (q *Queue) Dequeue() string {
+	if q.Head == nil {
+		return ""
+	}
+
+	node := q.Head
+	q.Head = q.Head.Next
+
+	return node.Val
+}
